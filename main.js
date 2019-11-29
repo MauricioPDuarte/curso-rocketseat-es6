65980c9d2f2faf1218d4b14e5ab699dc146c7f48
Mauricio Duarte <prussdev@gmail.com>
2019-11-29 17:06:17 -0300

    Operadores rest/spread

--- main.js
+++ main.js
@@ -1,21 +1,55 @@
+// REST
+//Pegar o resto das propriedades
+
+//1
+
 const usuario = {
     nome: 'Diego',
     idade: 23,
-    endereco: {
-        cidade: 'Rio do Sul',
-        estado: 'SC'
-    }
-}
+    empresa: 'Rocketseat'
+};
 
-/*
-const { nome, idade, endereco: { cidade } } = usuario;
+const { nome, ...resto } = usuario;
 
 console.log(nome);
-console.log(idade);
-console.log(cidade);
-*/
+console.log(resto);
 
-function mostraNome({ nome,idade }) {
-    console.log(nome, idade)
+const arr = [1, 2, 3, 4];
+
+const [a, b, ...c] = arr;
+
+console.log(a, b, c);
+
+//2
+
+function soma(a, b, ...params) {
+    return params;
 }
-mostraNome(usuario);
+
+console.log(soma(1, 3, 4));
+
+// SPREAD
+//Propagar as informaçoes pra outra estrutura de dados...
+
+
+//1
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [...arr1, ...arr2];
+
+console.log(arr3);
+
+//2
+const usuario1 = {
+    nome: 'Diego',
+    idade: 23,
+    empresa: 'Rocketseat'
+};
+
+const usuario2 = {
+    ...usuario1, nome: 'Gabriel'
+}
+
+console.log(usuario2);
+
