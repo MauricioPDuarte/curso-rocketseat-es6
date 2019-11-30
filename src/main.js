31f89e7655d07a20063f630fde073002ac231711
Mauricio Duarte <prussdev@gmail.com>
2019-11-30 18:00:25 -0300

    Configurando axios

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-const minhaPromise = () => new Promise((resolve, reject) => {
-    setTimeout(() => { resolve('OK') }, 2000);
-});
+import axios from 'axios';
 
+class Api {
+    static async getUserInfo(username) {
+        try{
+            const response = await axios.get(`https://api.github.com/users/${username}`)
+            console.log(response);
+        }catch(err) {
+            console.warn('Erro na API');
+        }
 
-// Async/await
-
-async function executaPromise() {
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
+    }
 }
 
-executaPromise();
+Api.getUserInfo('MauricioPDuarte')
+Api.getUserInfo('Mauriciouarte')
+
