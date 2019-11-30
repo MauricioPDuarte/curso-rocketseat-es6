17f529a43809836fcffd736bc1a6db693a497187
Mauricio Duarte <prussdev@gmail.com>
2019-11-30 18:19:47 -0300

    Desafio Async/await

--- src/main.js
+++ src/main.js
@@ -1,17 +1,133 @@
+/* Exercicio */
+
+//1
+// Funão delay aciona o .then após 1s
+
+/* Padrão
+const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+function umPorSegundo() {
+    delay().then(() => {
+        console.log('1s');
+        delay().then(() => {
+            console.log('2s');
+            delay().then(() => {
+                console.log('3s');
+            });
+        })
+    });
+}
+umPorSegundo();
+*/
+
+//Novo
+/*
+const deley = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+async function umPorSegundo() {
+    await deley()
+    console.log('1s');
+    await deley()
+    console.log('2s');
+    await deley()
+    console.log('3s');
+}
+
+umPorSegundo();
+*/
+
+//2
+
+/* Padrao
+
+import axios from 'axios';
+function getUserFromGithub(user) {
+    axios.get(`https://api.github.com/users/${user}`)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(err => {
+            console.log('Usuário não existe');
+        })
+}
+getUserFromGithub('diego3g');
+getUserFromGithub('diego3g124123');
+
+*/
+
+//Novo
 import axios from 'axios';
 
-class Api {
-    static async getUserInfo(username) {
-        try{
-            const response = await axios.get(`https://api.github.com/users/${username}`)
-            console.log(response);
-        }catch(err) {
-            console.warn('Erro na API');
-        }
-
+async function getUserFromGithub(user) {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${user}`);
+        console.log(response.data)
+    } catch (err) {
+        console.warn('Erro na API');
     }
 }
 
-Api.getUserInfo('MauricioPDuarte')
-Api.getUserInfo('Mauriciouarte')
+getUserFromGithub('MauricioPDuarte');
 
+//3
+
+/* Padrao
+class Github {
+    static getRepositories(repo) {
+        axios.get(`https://api.github.com/repos/${repo}`)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(err => {
+                console.log('Repositório não existe');
+            })
+    }
+}
+Github.getRepositories('rocketseat/rocketseat.com.br');
+Github.getRepositories('rocketseat/dslkvmskv');
+*/
+
+//Novo
+/*
+class Github {
+    static async getRepositories(repo) {
+        try {
+            const response = await axios.get(`https://api.github.com/repos/${repo}`);
+            console.log(response.data);
+        } catch (err) {
+            console.warn('Repositório não existe');
+        }
+    }
+}
+
+Github.getRepositories('rocketseat/rocketseat.com.br');
+Github.getRepositories('rocketseat/dslkvmskv');
+*/
+
+//4
+
+/* Padrao
+const buscaUsuario = usuario => {
+    axios.get(`https://api.github.com/users/${user}`)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(err => {
+            console.log('Usuário não existe');
+        });
+}
+buscaUsuario('diego3g');
+*/
+
+//Novo
+const buscaUsuario = async user => {
+    try{
+        const response = await axios.get(`https://api.github.com/users/${user}`);
+        console.log(response.data);
+    }catch(err) {
+        console.log(err)
+        console.warn('Usuário não existente');
+    }
+
+}
+
+buscaUsuario('diego3g');
