ab90e222de4c929a8c5306e0c732a230997b1a4a
Mauricio Duarte <prussdev@gmail.com>
2019-11-30 17:24:09 -0300

    Import/export

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-import { soma } from './funcoes';
+import * as funcoes from './funcoes';
+import somaFunction from './soma';
 
-console.log(soma(1, 2));
+console.log(funcoes.soma(1, 2));
+console.log(funcoes.sub(1, 2));
+console.log(funcoes.mult(1, 2));
+
+console.log(somaFunction(1, 2));
