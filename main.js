84b2523c61871b17280414f1aae61cd5caaa7403
Mauricio Duarte <prussdev@gmail.com>
2019-11-30 17:39:47 -0300

    Desafio Webpack Server

--- main.js
+++
@@ -1,8 +0,0 @@
-import * as funcoes from './funcoes';
-import somaFunction from './soma';
-
-console.log(funcoes.soma(1, 2));
-console.log(funcoes.sub(1, 2));
-console.log(funcoes.mult(1, 2));
-
-console.log(somaFunction(1, 2));
